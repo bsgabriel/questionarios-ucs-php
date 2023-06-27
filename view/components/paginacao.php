3ee910bccb8c3e70b099bed2a0771bb4fa8dddb5
bsgabriel <gabrielbs22@icloud.com>
2023-06-26 22:52:44 -0300

    criado componente para paginação

---
+++ view/components/paginacao.php
@@ -0,0 +1,17 @@
+<div class="container">
+ <ul id="paginacao" class="pagination justify-content-center">
+  <li class="page-item">
+   <button class="page-link">voltar</button>
+  </li>
+  <li class="page-item disabled">
+   <span class="page-link">1</span>
+  </li> <!-- Número da página -->
+  <li class="page-item">
+   <button class="page-link">avançar</button>
+  </li>
+ </ul>
+</div>
+<!--
+ script de paginação
+<script src="paginacao.js"></script>
+-->
